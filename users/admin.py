44607999d032dfaa7e8bab7c11cfd79a181910b4
Alex White <alex.white456@gmail.com>
2015-05-26 21:41:59 -0700

    Custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
-# Register your models here.
+from users.models import User
+from users.forms import UserChangeForm, UserCreationForm
+
+from django.contrib.auth.admin import UserAdmin
+
+class AddictionUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('display_name', 'email', )
+    ordering = ('display_name', )
+
+    fieldsets = (
+        (None, {'fields': ('display_name', 'email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('display_name', 'email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, AddictionUserAdmin)
