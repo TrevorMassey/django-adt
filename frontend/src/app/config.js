7364e1aca3c538e685935e809f937b3e3462e59b
Ara Hacobian <unkleara@gmail.com>
2015-06-07 22:11:19 -0700

    remove js-data dependency for now

--- frontend/src/app/config.js
+++ frontend/src/app/config.js
@@ -19,7 +19,7 @@ var ApplicationConfiguration = (function() {
 		'ncy-angular-breadcrumb',
 		'angular-loading-bar',
 		'restangular',
-		'js-data',
+		//'js-data',
 		//'ui.tree',
 		'ngFitText',
 		'toastr'
