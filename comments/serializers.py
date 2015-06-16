5ecac234c73f9e61b5ade8eaa5a024e261100c9d
Alex White <alex.white456@gmail.com>
2015-06-15 21:26:54 -0700

    Add readonly fields to comment serialzier

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,21 +1,30 @@
+import logging
+
 from rest_framework import serializers
 from comments.models import Comment
 from users.serializers import BasicUserSerializer
 
+logger = logging.getLogger(__name__)
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = BasicUserSerializer()
-    deleted_by = BasicUserSerializer()
+    user = BasicUserSerializer(read_only=True)
+    deleted_by = BasicUserSerializer(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('body',
-                  'created',
-                  'public',
-
-                  'is_deleted',
-                  'deleted_by',
-                  'deleted_time',
-
-                  'user',
-                  )
+        fields = (
+            'id',
+            'body',
+            'user',
+            'deleted_by',
+            'created',
+            'public',
+            'is_deleted',
+        )
+        read_only_fields = (
+            'id',
+            'public',
+            'is_deleted',
+            'deleted_time',
+            'created',
+        )
