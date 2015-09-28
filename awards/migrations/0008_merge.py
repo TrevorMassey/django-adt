5734db2d883ce2f5c99e0fc0c4e821114d842e11
Alex White <alex.white456@gmail.com>
2015-09-27 19:59:24 -0700

    Add merge migrations

---
+++ awards/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('awards', '0007_auto_20150625_0215'),
+        ('awards', '0006_auto_20150626_0127'),
+    ]
+
+    operations = [
+    ]
