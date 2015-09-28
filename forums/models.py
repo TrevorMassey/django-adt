4469455c974c1cd3019f15a9847a136625c77647
Alex White <alex.white456@gmail.com>
2015-09-27 21:34:55 -0700

    Add forum model methods

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models, transaction
 from django.utils import timezone
 from mptt.fields import TreeForeignKey
@@ -99,22 +100,14 @@ class Topic(models.Model):
     # Topic starter
     first_post = models.ForeignKey('forums.Post', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
     starter = models.ForeignKey('users.User', null=True, blank=True, on_delete=models.SET_NULL)
-    starter_name = models.CharField(max_length=255)
-    starter_avatar = models.CharField(max_length=255, null=True, blank=True)
-    starer_rank_color = models.CharField(max_length=255, null=True, blank=True)
 
     # Latest reply
     last_post_on = models.DateTimeField(db_index=True)
     last_post = models.ForeignKey('forums.Post', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
-    last_poster = models.ForeignKey('users.User', related_name='last_poster_set', null=True, blank=True, on_delete=models.SET_NULL)
-    last_poster_name = models.CharField(max_length=255, null=True, blank=True)
-    last_poster_avatar = models.CharField(max_length=255, null=True, blank=True)
-    last_poster_rank_color = models.CharField(max_length=255, null=True, blank=True)
 
     # Deleted
     is_deleted = models.BooleanField(default=False)
     deleted_by = models.ForeignKey('users.User', blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
-    deleted_by_name = models.CharField(max_length=255, null=True, blank=True)
     deleted_time = models.DateTimeField(blank=True, null=True)
 
     objects = TopicManager()
@@ -138,6 +131,10 @@ class Topic(models.Model):
         self.deleted_time = timezone.now()
         self.save()
 
+    def reply(self, poster, body):
+        post = Post.objects.create_post(poster=poster, topic=self, body=body)
+        return post
+
 
 class PostManager(models.Manager):
 
@@ -147,11 +144,10 @@ class PostManager(models.Manager):
         """
 
         post = Post()
+        post.forum = topic.forum
         post.thread = topic
         post.body = body
         post.poster = poster
-        post.poster_name = poster.display_name
-        post.poster_avatar = poster.avatar
         post.save()
 
         return post
@@ -159,12 +155,7 @@ class PostManager(models.Manager):
 class Post(models.Model):
     forum = models.ForeignKey('forums.Forum')
     thread = models.ForeignKey('forums.Topic')
-
     poster = models.ForeignKey('users.User', null=True, blank=True, on_delete=models.SET_NULL)
-    poster_name = models.CharField(max_length=255)
-    poster_avatar = models.CharField(max_length=255, null=True, blank=True)
-    poster_rank_color = models.CharField(max_length=255, null=True, blank=True)
-    poster_signature = models.CharField(max_length=255, null=True, blank=True)
 
     body = models.TextField()
 
@@ -176,14 +167,10 @@ class Post(models.Model):
     updated = models.DateTimeField(blank=True, null=True)
     edits = models.PositiveIntegerField(default=0)
     last_editor = models.ForeignKey('users.User', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
-    last_editor_name = models.CharField(max_length=255, null=True, blank=True)
-    last_editor_avatar = models.CharField(max_length=255, null=True, blank=True)
-    last_editor_rank_color = models.CharField(max_length=255, null=True, blank=True)
 
     # Deleted
     is_deleted = models.BooleanField(default=False)
     deleted_by = models.ForeignKey('users.User', related_name='+', blank=True, null=True, on_delete=models.SET_NULL)
-    deleted_by_name = models.CharField(max_length=255, null=True, blank=True)
     deleted_time = models.DateTimeField(blank=True, null=True)
 
     objects = PostManager()
@@ -196,6 +183,15 @@ class Post(models.Model):
 
     def __unicode__(self):
         return '%s...' % self.original[10:].strip()
+
+    def edit(self, body, editor):
+
+        post = Post.objects.select_for_update().get(id=self.id)
+        post.body = body
+        post.edits += 1
+        post.last_editor = editor
+        post.updated = timezone.now()
+        post.save()
 
     def soft_delete(self, user):
         self.deleted_by = user
